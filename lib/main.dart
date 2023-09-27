14466d1a5d903fd84ed58614810fc7d9cc4369fc
aluno <aluno@ifpe>
2023-09-27 11:53:07 -0300

    Parte 1 do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/HomePage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() =>  runApp( MyApp());
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
- 
+  //const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        "/":(context) => HomePage(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
      
